7b1fa905b86064fcd1e4a9f766d8c727f39695df
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-21 11:20:46 -0300

    console for pubkey in balance for main

--- packages/web-ui/src/app/@core/services/balance.service.ts
+++ packages/web-ui/src/app/@core/services/balance.service.ts
@@ -136,7 +136,7 @@ private async updateCoinBalanceForAddressFromWallet(address: string, pubkey?: st
 
     try {
       const payload = { pubkey };
-      console.log('balance utxo query '+`${this.url}/address/utxo/${address}`)
+      console.log('balance utxo query '+`${this.url}/address/utxo/${address}` +JSON.stringify(payload))
       const { data: unspentUtxos } = await axios.post(`${this.url}/address/utxo/${address}`, payload);
 
 
