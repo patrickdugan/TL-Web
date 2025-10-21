012c49a57cc7b5d954116a106ac116787feb5113
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-10-21 13:12:43 -0300

    configs fix

--- packages/web-ui/src/environments/environment.prod.ts
+++ packages/web-ui/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ import { ENDPOINTS } from "./endpoints.conf";
 export const environment = {
   production: true,
   apiBase: '/api',       // or your deployed API URL
-  algoDefaults: true
+  algoDefaults: true,
+  homeApiUrl: "http://localhost:1986",
+  ENDPOINTS,
 };
--- packages/web-ui/src/environments/environment.ts
+++ packages/web-ui/src/environments/environment.ts
@@ -3,6 +3,8 @@ import { ENDPOINTS } from "./endpoints.conf";
 export const environment = export const environment = {
   production: false,
   apiBase: '/api',       // where your BE routes are mounted
-  algoDefaults: true     // preload default algos if index is empty
+  algoDefaults: true,     // preload default algos if index is empty
+  homeApiUrl: "http://localhost:1986",
+  ENDPOINTS,
 };
 
