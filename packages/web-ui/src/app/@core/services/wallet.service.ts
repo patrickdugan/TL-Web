ace1a406388eb79418da2e818c836aa7db2c4b24
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2026-05-25 21:02:09 -0400

    Revert "Use account request for TradeLayer connect"
    This reverts commit a30b99dc23b4e153cba8e10d331972a623248f4c.

--- packages/web-ui/src/app/@core/services/wallet.service.ts
+++ packages/web-ui/src/app/@core/services/wallet.service.ts
@@ -874,9 +874,10 @@ export class WalletService {
     isAvailable: () => hasTradeLayerWallet(),
 
     connect: async () => {
-      // The Chrome Web Store extension does not support a separate `connect`
-      // method. Its user gesture is `requestAccounts`, which also returns the
-      // address for the selected LTC/LTCTEST network.
+      if (getTradeLayerProvider()) {
+        return requestTradeLayerConnect(this.customWalletNetwork());
+      }
+
       return requestTradeLayerAccounts(this.customWalletNetwork());
     },
 
@@ -1023,6 +1024,9 @@ export class WalletService {
     const cachedAddresses = this.addresses$.value || [];
     if (cachedAddresses.length) {
       return cachedAddresses.map((address) => ({ address }));
+    }
+    if (!this.provider$.value) {
+      return [];
     }
     const activeProvider = this.provider$.value || this.pick();
     const phantomBtc = getPhantomBtc(normalizedNetwork);
